2b4df8a6ba1eeb680f7bb080c06c6ca1007d3183
nguyentk1272 <80952748+nguyentk1272@users.noreply.github.com>
2022-07-15 15:40:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-
 include "online-shopping-system/index.php";
+include "online-shopping-system/header.php";
+
+include "online-shopping-system/body.php";
+include "online-shopping-system/newslettter.php";
+include "online-shopping-system/footer.php";
 ?>
+		
+		
+
 		
 		
